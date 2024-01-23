4cfbfdca4b0178767849d65fb0eceb26fa458aa9
YoHaiYo <12si47bun@naver.com>
2024-01-23 11:18:01 +0900

    navi에서 json 전달해보기

--- server/routers/navi.js
+++ server/routers/navi.js
@@ -1,28 +1,44 @@
 const express = require("express")
 const naviapp = express.Router();
 
+const myjson = {
+  "ㅎㅎ": 11,
+  "ㅋㅋ": true,
+  "LOL": ":)"
+}
+
+// import myhtml from '../../www/test.html'
+
 //중요 (라우터의 쿼리스트링을 일반문자가 아닌 데이터로 인식하게 하기 )
-naviapp.use(express.urlencoded({ extended: true }))
+naviapp.use(express.json())
+// req.query 변수 가져오기 위해서
+naviapp.use(express.urlencoded({ extended: true })) // true로 해야 범용성이 높다고 한다.
 
 naviapp.get('/', (req, res, next) => {
   const info = req.query.tablenm;  // pc
   const no = req.query.num;  // 1   
   if (info == "navi") {
+    // next('route');// 다음라우터 실행
+
+    // next 다음 send 나오면 통신종료!
     res.send(`${info}테이블 데이터 쿼리문 돌려서 보낸준 자료
-          http://localhost:8080/data?tablenm=navi
+          http://localhost:3333/data?tablenm=navi
           `)
   } else if (info == "swiper") {
-    res.send(`${info}테이블 http://localhost:8080/data?tablenm=swiper`)
-  } else if (info == "qna") {
-    res.send(`${info}테이블 노드자료를 리액트에게 전달`)
+    res.send(`${info}테이블 http://localhost:3333/data?tablenm=swiper`)
+  } else if (info == "json") {
+    // http://localhost:3333/data?tablenm=json
+    res.send(myjson)
   } else if (info == "test") {
     // res.send(`${info}테이블 노드자료를 리액트에게 전달`)    
-    res.sendFile('../../www/test.html')
+    // res.send(myhtml)
+    res.send(null)
   }
+
 
   if (no == 1) {
     res.send(`
-          http://localhost:8080/data?num=${no} 
+          http://localhost:3333/data?num=${no} 
           입니다.         
           `)
   }
--- server/web.js
+++ server/web.js
@@ -1,9 +1,29 @@
 const express = require('express')
 const path = require('path')
-const port = process.env.PORT || 3333
+const port = process.env.PORT || 3333;
 const navidata = require("./routers/navi");
 
 const app = express();
+
+// 전역변수 선언
+// set안에 선언시 전역변수로 바뀜 ! (다른 파일에서도 쓸 수 있어요.)
+app.set(port, process.env.PORT || 3333);
+// 전역변수 가져올때
+app.get(app.set(port));
+
+app.get("/datasend", (req, res, next) => { // post 대신 use쓰면 get, post 아무거나 된다.
+  // ... 작업실행
+  // req.body -> 절대안됨. (데이터가 body를 타고 오기 때문에 건들지 마쇼. 여기서 body는 html이 아니라 post의 head, body)
+  req.mydata = "ssh";
+
+  next();
+}, (req, res, next) => {
+  console.log(req.body, req.mydata)
+  // req.body, req.mydata
+  res.send("이제 끝");
+  // res.send 이후 실행식은 실행안된다.
+
+});
 
 app.use(express.static(path.join(__dirname, '../www')))
 
